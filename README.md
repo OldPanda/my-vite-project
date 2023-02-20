ee130272e6697b8f31f384b7b6f9a2fdb08c6a14
OldPanda <me@old-panda.com>
2023-02-19 20:18:47 -0800

    Init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# My Vite + DaisyUI Starter Project
+
+This is the starter project using Vite and DaisyUI for frontend learner. The construction steps can be found at [this post](https://old-panda.com/2023/02/12/how-to-quickly-setup-a-frontend-project-using-vite-and-daisyui/).
+
+## Project Setup
+```
+pnpm install
+```
+
+## Compile and Hot-Reload for Development
+```
+pnpm run dev
+```
+
+## Type-Check, Compile and Minify for Production
+```
+pnpm run build
+```
+
+## Run Unit Tests with Vitest
+```
+pnpm run test:unit
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="card w-1/3 bg-base-100 shadow-xl">
+    <figure><img src="https://evercam.com/wp-content/uploads/sites/11/2019/11/big-red-button.jpg" alt="Red Button" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">{{ msg }}</h2>
+      <p>This is a magic button showing how many clicks on it</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary" @click="count++">count is {{ count }}</button>
+      </div>
+    </div>
+  </div>
+</template>
